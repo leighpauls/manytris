55f02d1dfacb6d68f4ec4a9065c17492c95a3b0a
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2024-08-19 23:05:24 -0700

    Process gpu moves in batches

--- src/bot_shader.rs
+++ src/bot_shader.rs
@@ -36,8 +36,12 @@ impl BotShaderContext {
 
     pub fn evaluate_moves(
         &self,
-        moves: &Vec<MovementBatchRequest>,
+        batches: &Vec<MovementBatchRequest>,
     ) -> Result<Vec<MovementBatchResult>, String> {
+        let initial_states = batches.len();
+        let num_moves = batches.iter().map(|b| b.moves.len()).sum();
+
+        /*
         moves
             .iter()
             .map(|m| {
@@ -45,53 +49,53 @@ impl BotShaderContext {
                 Ok(MovementBatchResult { result: batch })
             })
             .collect()
-    }
 
-    pub fn evaluate_single_movement_batch(
-        &self,
-        src_state: &BitmapField,
-        moves: &Vec<MovementDescriptor>,
-    ) -> Result<Vec<(BitmapField, MoveResultScore)>, String> {
-        let initial_states = 1;
-        let num_moves = moves.len();
+         */
+
         let mut buffers = self.kc.make_buffers(initial_states, num_moves);
-        write_to_buffer(&mut buffers.fields, 0, src_state);
 
-        moves.iter().enumerate().for_each(|(i, md)| {
-            let cur_position_idx = i * 2;
-            write_to_buffer(
-                &mut buffers.positions,
-                cur_position_idx,
-                self.sp.bot_start_tps(md.shape, md.cw_rotations),
-            );
-            let next_position_idx = cur_position_idx + 1;
-            write_to_buffer(
-                &mut buffers.positions,
-                next_position_idx,
-                self.sp.player_start_tps(md.next_shape),
-            );
+        let mut move_idx = 0;
 
-            let output_field_idx = initial_states + i;
-            write_to_buffer(
-                &mut buffers.configs,
-                i,
-                &DropConfig {
-                    tetromino_idx: cur_position_idx as u32,
-                    next_tetromino_idx: next_position_idx as u32,
-                    initial_field_idx: 0,
-                    dest_field_idx: output_field_idx as u32,
-                    left_shifts: if md.shifts_right < 0 {
-                        (-md.shifts_right) as u8
-                    } else {
-                        0
+        batches.iter().enumerate().for_each(|(batch_idx, b)| {
+            write_to_buffer(&mut buffers.fields, batch_idx, &b.src_state);
+
+            b.moves.iter().for_each(|md| {
+                let cur_position_idx = move_idx * 2;
+                write_to_buffer(
+                    &mut buffers.positions,
+                    cur_position_idx,
+                    self.sp.bot_start_tps(md.shape, md.cw_rotations),
+                );
+                let next_position_idx = cur_position_idx + 1;
+                write_to_buffer(
+                    &mut buffers.positions,
+                    next_position_idx,
+                    self.sp.player_start_tps(md.next_shape),
+                );
+
+                let output_field_idx = initial_states + move_idx;
+                write_to_buffer(
+                    &mut buffers.configs,
+                    move_idx,
+                    &DropConfig {
+                        tetromino_idx: cur_position_idx as u32,
+                        next_tetromino_idx: next_position_idx as u32,
+                        initial_field_idx: batch_idx as u32,
+                        dest_field_idx: output_field_idx as u32,
+                        left_shifts: if md.shifts_right < 0 {
+                            (-md.shifts_right) as u8
+                        } else {
+                            0
+                        },
+                        right_shifts: if md.shifts_right > 0 {
+                            md.shifts_right as u8
+                        } else {
+                            0
+                        },
                     },
-                    right_shifts: if md.shifts_right > 0 {
-                        md.shifts_right as u8
-                    } else {
-                        0
-                    },
-                },
-            )
+                );
+                move_idx += 1;
+            });
         });
 
         self.kc.run_cmd(&buffers, num_moves)?;
@@ -100,10 +104,16 @@ impl BotShaderContext {
         let score_slice = slice_from_buffer::<MoveResultScore>(&buffers.scores);
         assert_eq!(result_slice.len(), score_slice.len());
 
-        Ok(result_slice
-            .iter()
+        let mut result_iter = result_slice
+            .into_iter()
             .zip(score_slice)
-            .map(|(b, s)| (b.clone(), s.clone()))
+            .map(|(b, s)| (b.clone(), s.clone()));
+
+        Ok(batches
+            .iter()
+            .map(|b| MovementBatchResult {
+                result: result_iter.by_ref().take(b.moves.len()).collect(),
+            })
             .collect())
     }
 }
